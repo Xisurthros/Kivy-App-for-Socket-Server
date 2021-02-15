97b1ee86bcdc97a98eb54581e95123a820796325
Xisurthros <76274780+Xisurthros@users.noreply.github.com>
2021-02-15 12:01:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
 import socket
-import select
-import errno
-import sys
-import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
-
-HEADER_LENGTH = 10
-IP = '127.0.0.1'
-PORT = 5000
-
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((IP, PORT))
-client_socket.setblocking(False)
 
 
 class MyGrid(Widget):
@@ -25,7 +9,7 @@ class MyGrid(Widget):
     email = ObjectProperty(None)
 
     def btn(self):
-        print(self.name.text)
+        # print(self.name.text)
         message = self.name.text
         if message:
             message = message.encode('utf-8')
@@ -36,15 +20,19 @@ class MyGrid(Widget):
                 message_header = client_socket.recv(HEADER_LENGTH)
                 message_length = int(message_header.decode('utf-8').strip())
                 message = client_socket.recv(message_length).decode('utf-8')
-                print(f'{message_length}')
-        except IOError as e:
-            if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-                print('Reading error ', str(e))
-                sys.exit()
-        except Exception as e:
-            print('General error', str(e))
-            sys.exit()
+                # print(f'{message_length}')
+        except:
+            pass
         self.name.text = ""
+
+
+HEADER_LENGTH = 10
+IP = '127.0.0.1'
+PORT = 5000
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((IP, PORT))
+client_socket.setblocking(False)
 
 
 class MyApp(App):
