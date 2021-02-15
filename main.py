ee13bf7e68318308ed3e215a3eb0b97400d9a431
Xisurthros <76274780+Xisurthros@users.noreply.github.com>
2021-02-15 16:30:55 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,34 +5,37 @@ from kivy.properties import ObjectProperty
 
 
 class MyGrid(Widget):
-    name = ObjectProperty(None)
-    email = ObjectProperty(None)
+    try:
+        HEADER_LENGTH = 10
+        IP = '127.0.0.1'
+        PORT = 5000
+
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((IP, PORT))
+        client_socket.setblocking(False)
+
+        name = ObjectProperty(None)
+        email = ObjectProperty(None)
+        
+    except:
+        pass
 
     def btn(self):
         # print(self.name.text)
         message = self.name.text
         if message:
             message = message.encode('utf-8')
-            message_header = f'{len(message):<{HEADER_LENGTH}}'.encode('utf-8')
-            client_socket.send(message_header + message)
+            message_header = f'{len(message):<{self.HEADER_LENGTH}}'.encode('utf-8')
+            self.client_socket.send(message_header + message)
         try:
             while True:
-                message_header = client_socket.recv(HEADER_LENGTH)
+                message_header = self.client_socket.recv(self.HEADER_LENGTH)
                 message_length = int(message_header.decode('utf-8').strip())
-                message = client_socket.recv(message_length).decode('utf-8')
+                message = self.client_socket.recv(message_length).decode('utf-8')
                 # print(f'{message_length}')
         except:
             pass
         self.name.text = ""
-
-
-HEADER_LENGTH = 10
-IP = '127.0.0.1'
-PORT = 5000
-
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((IP, PORT))
-client_socket.setblocking(False)
 
 
 class MyApp(App):
